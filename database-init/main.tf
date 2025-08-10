d469667ff67214ec2bee53ea0a752c4a9689b110
co <cockernutx@gmail.com>
2025-08-10 00:08:42 +0000

    Removed unused resources

--- database-init/main.tf
+++ database-init/main.tf
@@ -13,32 +13,3 @@ resource "postgresql_database" "services" {
   name     = each.value
   owner    = var.postgres_user
 }
-
-# Grant CONNECT privilege on all databases
-resource "postgresql_grant" "debezium_connect" {
-  for_each    = postgresql_database.services
-  database    = each.value.name
-  role        = var.debezium_user
-  object_type = "database"
-  privileges  = ["CONNECT"]
-}
-
-# Grant USAGE on schema for all databases
-resource "postgresql_grant" "debezium_schema" {
-  for_each    = postgresql_database.services
-  database    = each.value.name
-  role        = var.debezium_user
-  schema      = "public"
-  object_type = "schema"
-  privileges  = ["USAGE"]
-}
-
-# Grant SELECT on all tables for all databases
-resource "postgresql_grant" "debezium_tables" {
-  for_each    = postgresql_database.services
-  database    = each.value.name
-  role        = var.debezium_user
-  schema      = "public"
-  object_type = "table"
-  privileges  = ["SELECT"]
-}
